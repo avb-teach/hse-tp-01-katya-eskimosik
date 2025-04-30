3c3a0d43fa0121c918c57460497d4ce613faec31
katya-eskimosik <115539724+katya-eskimosik@users.noreply.github.com>
2025-05-01 01:32:42 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,8 +1,3 @@
-input_dir="$1"
-output_dir="$2"
-
-mkdir -p "$output_dir"
-
 python3 - <<EOF
 import os
 import shutil
@@ -15,12 +10,5 @@ for root, dirs, files in os.walk(input_dir):
     for file in files:
         file_path = os.path.join(root, file)
 
-        new_file_name = file
-        if new_file_name in file_count:
-            file_count[new_file_name] += 1
-            new_file_name = f"{os.path.splitext(file)[0]}{file_count[new_file_name]}{os.path.splitext(file)[1]}"
-        else:
-            file_count[new_file_name] = 1
-            
         shutil.copy(file_path, output_dir)
 EOF
