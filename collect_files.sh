6f7a6ea8c5846e7bef7ae6797c2c294ae0ace007
katya-eskimosik <115539724+katya-eskimosik@users.noreply.github.com>
2025-05-01 16:46:49 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-python3 - <<EOF
+input_dir="$1"
+output_dir="$2"
+
+mkdir -p "$output_dir"
+
+python3 - <<EOT
 import os
 import shutil
-import sys
 
-input_dir = sys.argv[1]
-output_dir = sys.argv[2]
+in_dir = os.path.abspath("$input_dir")
+out_dir = os.path.abspath("$output_dir")
 
-for root, dirs, files in os.walk(input_dir):
+file_count = {}
+
+for root, dirs, files in os.walk(in_dir):
     for file in files:
+    
         file_path = os.path.join(root, file)
+        name, per = os.path.splitext(file)
 
-        shutil.copy(file_path, output_dir)
-EOF
+        new_file_name = file
+
+         if new_file_name in file_count:
+             file_count[new_file_name] += 1
+             new_file_name = f"{name}{file_count[new_file_name]}{per}"
+         else:
+             file_count[new_file_name] = 1
+
+        shutil.copy(file_path, out_dir)
+EOT
